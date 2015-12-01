cb71e8deab3ce3cd53e7ba2825ebe5051f191c24
Michał Górniak <mgorniak@mirantis.com>
2015-12-01 12:30:41 +0100

    fix: update crm to make redeploy posible

--- deployment_scripts/puppet/modules/neutron/manifests/services/ha_agents.pp
+++ deployment_scripts/puppet/modules/neutron/manifests/services/ha_agents.pp
@@ -13,6 +13,26 @@ class neutron::services::ha_agents (
         }
     }
 
+    exec { 'Remove l3 dependancy colocation from CRM':
+      command => 'crm configure delete clone_p_neutron-l3-agent-with-clone_p_neutron-plugin-openvswitch-agent',
+      path    => '/usr/sbin:/bin:/sbin',
+      onlyif  => 'crm configure show | grep clone_p_neutron-l3-agent-with-clone_p_neutron-plugin-openvswitch-agent',
+    }
+
+    exec { 'Remove l3 dependancy order from CRM':
+      command => 'crm configure delete clone_p_neutron-l3-agent-after-clone_p_neutron-plugin-openvswitch-agent',
+      path    => '/usr/sbin:/bin:/sbin',
+      onlyif  => 'crm configure show | grep clone_p_neutron-l3-agent-after-clone_p_neutron-plugin-openvswitch-agent',
+    }
+
+    package { 'neutron-plugin-openvswitch-agent':
+      ensure => 'purged',
+    }
+
+    package { 'neutron-l3-agent':
+      ensure => 'purged',
+    }
+
     service {'neutron-dhcp-agent':
         ensure     => $service_ensure,
         name       => $::neutron::params::ha_dhcp_agent,
@@ -53,6 +73,30 @@ class neutron::services::ha_agents (
         tag        => 'ha_agents',
     }
 
+
+    exec { 'kill p_neutron-l3-agent':
+      command => 'killall neutron-l3-agent',
+      path    => '/usr/sbin:/bin:/sbin:/usr/bin',
+      onlyif  => 'pgrep  -f neutron-l3-agent',
+    }
+
+    exec { 'Cleanup p_neutron-l3-agent':
+      command => 'pcs resource clear p_neutron-l3-agent',
+      path    => '/usr/sbin:/bin:/sbin:/usr/bin',
+      onlyif  => 'crm configure show | grep  cli-ban-p_neutron-l3-agent',
+    }
+
+    exec { 'Cleanup p_neutron-plugin-openvswitch-agent':
+      command => 'pcs resource clear p_neutron-plugin-openvswitch-agent',
+      path    => '/usr/sbin:/bin:/sbin:/usr/bin',
+      onlyif  => 'crm configure show | grep cli-ban-p_neutron-plugin-openvswitch-agent',
+    }
+
+    exec { 'restart openvswitch restart':
+      command => 'service openvswitch-switch restart',
+      path    => '/usr/sbin:/bin:/sbin:/usr/bin',
+      onlyif  => '/etc/init.d/openvswitch-switch status',
+    }
 
     Neutron_config<||>              ~> Service<| tag == 'ha_agents' |>
     Neutron_plugin_ml2<||>          ~> Service<| tag == 'ha_agents' |>
