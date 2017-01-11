e2840b2c2cd7fe91be806b8dd1f44a762dc86962
Ratnakar Kolli <rkolli@noironetworks.com>
2017-01-11 10:40:44 -0800

    pcs resource name for neutron-openvswitch-agent has changed.

--- deployment_scripts/puppet/modules/cisco_aci/manifests/disable_openvswitch_agent.pp
+++ deployment_scripts/puppet/modules/cisco_aci/manifests/disable_openvswitch_agent.pp
@@ -10,5 +10,14 @@ class cisco_aci::disable_openvswitch_agent (
      onlyif => "/usr/sbin/pcs resource show | grep -q clone_p_neutron-openvswitch-agent",
      require => Exec['disable_openvswitch_plugin'],
   }
+  exec {'disable_openvswitch_plugin2':
+     command  => "/usr/sbin/pcs resource disable clone_neutron-openvswitch-agent",
+     onlyif => "/usr/sbin/pcs resource show | grep -q clone_neutron-openvswitch-agent",
+  }
+  exec {'delete_openvswitch_plugin2':
+     command  => "/usr/sbin/pcs resource delete clone_neutron-openvswitch-agent",
+     onlyif => "/usr/sbin/pcs resource show | grep -q clone_neutron-openvswitch-agent",
+     require => Exec['disable_openvswitch_plugin2'],
+  }
          
 }
